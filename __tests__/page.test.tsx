6242610bc048a2a905d73cdb3a15f14452454731
clyde-yoonjae <leewh77777@gmail.com>
2025-02-11 18:03:25 +0900

    feat[DEVING-31]/endofline-lf로 모든파일 수정

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,6 +1,6 @@
+import Home from '@/app/page';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import Home from '@/app/page';
 
 describe('Page', () => {
   it('renders a heading', () => {
