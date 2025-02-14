beeda2a27fe2290a434c52befe3e6eb3a808fa35
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-14 15:16:35 +0900

    feat(DEVING-34): disabled, success, error 상태 구현

--- src/app/preview/input/page.tsx
+++ src/app/preview/input/page.tsx
@@ -3,8 +3,22 @@ import { Input } from '@/components/ui/Input';
 export default function ButtonExamples() {
   return (
     <div className="w-fill h-96 bg-BG p-10">
-      <div className="w-[460px]">
+      <div className="mb-4 w-[460px]">
+        <Input placeholder="Default" isValid />
+      </div>
+
+      <div className="mb-4 w-[460px]">
+        <Input
+          placeholder="Default"
+          isValid={false}
+          errorMessage="필수 입력 사항입니다."
+        />
+      </div>
+      <div className="mb-4 w-[460px]">
         <Input placeholder="Default" />
+      </div>
+      <div className="mb-4 w-[460px]">
+        <Input placeholder="Default" disabled />
       </div>
     </div>
   );
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,37 @@
 import { cn } from '@/util/cn';
 import * as React from 'react';
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  ({ className, type, ...props }, ref) => {
-    // 기본 fill,
+interface IInputProps extends React.ComponentProps<'input'> {
+  isValid?: boolean;
+  errorMessage?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, IInputProps>(
+  ({ className, type, isValid, errorMessage, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex w-full rounded-md border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-          'typo-button1 bg-Cgray200 px-[16px] py-[14px] text-Cgray700 placeholder:text-Cgray400',
-          className,
+      <div className="w-full">
+        <input
+          type={type}
+          className={cn(
+            'flex w-full rounded-md border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring md:text-sm',
+            'typo-button1 box-border h-[50px] bg-Cgray200 px-[16px] py-[14px] text-Cgray700 placeholder:text-Cgray400',
+            'disabled:cursor-not-allowed disabled:bg-disable disabled:text-disable_text',
+            isValid === undefined
+              ? ''
+              : isValid
+                ? 'border border-main'
+                : 'border border-warning text-warning',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {errorMessage && (
+          <p className="typo-caption1 mt-[10px] px-[10px] text-warning">
+            {errorMessage}
+          </p>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
   },
 );
