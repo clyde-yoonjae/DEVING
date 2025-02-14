8044649f60649aee7d0ba1ac335ba4827d60c8b3
lee1nna <dlgkssk0209@gmail.com>
2025-02-14 09:23:54 +0900

    feat(DEVING-16): shadcn/ui 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss';
+import { PluginAPI } from 'tailwindcss/types/config';
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -27,8 +29,114 @@ export default {
         Cgray800: '#D8DEE8',
         BG: '#121212',
         BG_2: '1B1B1D',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }: PluginAPI) {
+      addComponents({
+        '.typo-head1': {
+          fontSize: '32px',
+          lineHeight: '41px',
+          fontWeight: '600',
+        },
+        '.typo-head2': {
+          fontSize: '22px',
+          lineHeight: '28px',
+          fontWeight: '600',
+        },
+        '.typo-head3': {
+          fontSize: '17px',
+          lineHeight: '22px',
+          fontWeight: '600',
+        },
+        '.typo-head4': {
+          fontSize: '15px',
+          lineHeight: '20px',
+          fontWeight: '500',
+        },
+        '.typo-button1': {
+          fontSize: '16px',
+          lineHeight: '22px',
+          fontWeight: '600',
+        },
+        '.typo-button2': {
+          fontSize: '14px',
+          lineHeight: '16px',
+          fontWeight: '500',
+        },
+        '.typo-caption1': {
+          fontSize: '12px',
+          lineHeight: '16px',
+          fontWeight: '500',
+        },
+        '.typo-caption2': {
+          fontSize: '11px',
+          lineHeight: '13px',
+          fontWeight: '500',
+        },
+        '.typo-body1': {
+          fontSize: '15px',
+          lineHeight: '20px',
+          fontWeight: '400',
+        },
+        '.typo-body2': {
+          fontSize: '13px',
+          lineHeight: '18px',
+          fontWeight: '400',
+        },
+        '.typo-small': {
+          fontSize: '11px',
+          lineHeight: '13px',
+          fontWeight: '400',
+        },
+      });
+    },
+    require('tailwindcss-animate'),
+  ],
 } satisfies Config;
