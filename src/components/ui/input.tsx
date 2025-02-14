dd268edb4d38d288f6a9badb1b4c0e7c3246965c
dbswl701 <dbswl701@ajou.ac.kr>
2025-02-14 15:22:24 +0900

    style(DEVING-34): caret 색상 변경

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -15,12 +15,12 @@ const Input = React.forwardRef<HTMLInputElement, IInputProps>(
           className={cn(
             'flex w-full rounded-md border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring md:text-sm',
             'typo-button1 box-border h-[50px] bg-Cgray200 px-[16px] py-[14px] text-Cgray700 placeholder:text-Cgray400',
-            'disabled:cursor-not-allowed disabled:bg-disable disabled:text-disable_text',
+            'caret-Cgray500 disabled:cursor-not-allowed disabled:bg-disable disabled:text-disable_text',
             isValid === undefined
               ? ''
               : isValid
                 ? 'border border-main'
-                : 'border border-warning text-warning',
+                : 'border border-warning text-warning caret-warning',
             className,
           )}
           ref={ref}
