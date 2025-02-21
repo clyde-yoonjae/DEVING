023bd5076b36e8c81fa36964be65f0b1adc4c47c
clyde-yoonjae <leewh77777@gmail.com>
2025-02-21 16:40:25 +0900

    chore(DEVING-45): progress-bar tsDoc위치 이동

--- src/components/ui/Progress.tsx
+++ src/components/ui/Progress.tsx
@@ -25,6 +25,19 @@ interface ProgressProps
   total: number;
   value: number;
 }
+
+/**
+ * 기본적인 프로그래스바 컴포넌트입니다.
+ * @example
+ * // 기본 사용법
+ * <Progress total={50} value={33} />
+ *
+ * @param {object} props
+ * @param {string} [props.className] - Tailwind CSS 클래스를 통한 커스텀 스타일
+ * @param {number} [props.total] - 전체 값
+ * @param {number} [props.value] - 현재 진행된 값
+ */
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   ProgressProps
