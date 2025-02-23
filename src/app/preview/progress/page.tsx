0f4040811f366f0a58ecde793b8295da0d318eba
clyde-yoonjae <leewh77777@gmail.com>
2025-02-23 23:15:52 +0900

    test(DEVING-45): 리뷰컴포넌트 테스트 페이지

--- src/app/preview/progress/page.tsx
+++ src/app/preview/progress/page.tsx
@@ -1,13 +1,25 @@
 'use client';
 
+import ReviewAvgCard from '@/components/common/review/ReviewAvgCard';
 import { Progress } from '@/components/ui/Progress';
 import React from 'react';
 
 export default function ProgressExamples() {
+  const reviewData = {
+    average: 4.7,
+    count: {
+      fives: 999,
+      fours: 12,
+      threes: 0,
+      twos: 88,
+      ones: 3,
+    },
+  };
   return (
-    <div className="mt-3 flex flex-col">
-      <Progress className="w-40" total={50} value={33} />
-      <Progress total={100} value={70} />
+    <div className="mt-3 flex h-screen flex-col bg-BG p-8">
+      <Progress className="w-40" total={50} value={33} showCounter={true} />
+      <Progress total={100} value={70} showCounter={true} />
+      <ReviewAvgCard count={reviewData.count} />
     </div>
   );
 }
