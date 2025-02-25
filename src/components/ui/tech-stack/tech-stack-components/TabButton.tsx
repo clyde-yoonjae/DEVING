65eea8c08e9bf2b5c784627deb50973c3170834e
clyde-yoonjae <leewh77777@gmail.com>
2025-02-25 20:41:27 +0900

    feat(DEVING-49): 기술스택 카테고리 버튼생성

---
+++ src/components/ui/tech-stack/tech-stack-components/TabButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface TabButtonProps {
+  active: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+  smallText: string;
+}
+
+const TabButton = ({
+  active,
+  onClick,
+  children,
+  smallText,
+}: TabButtonProps): JSX.Element => {
+  return (
+    <button
+      className={`px-2 py-2 font-medium sm:px-4 ${
+        active
+          ? 'border-b-2 border-[#C586C0] text-[#C586C0]'
+          : 'hover:text-Cgray500'
+      }`}
+      onClick={onClick}
+    >
+      <span className="hidden sm:inline">{children}</span>
+      <span className="sm:hidden">{smallText}</span>
+    </button>
+  );
+};
+
+export default TabButton;
