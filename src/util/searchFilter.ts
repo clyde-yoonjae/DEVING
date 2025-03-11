8a5e1db7186850a4c501ff07abcdd595d84bf471
dbswl701 <dbswl701@ajou.ac.kr>
2025-03-11 17:28:02 +0900

    feat(DEVING-76): 모임 리스트 페이지 모달 parallel route 적용

--- src/util/searchFilter.ts
+++ src/util/searchFilter.ts
@@ -29,3 +29,18 @@ export const translateCategoryNameToKor = (category: string): CategoryTitle => {
       return '모각코';
   }
 };
+
+export const translateCategoryNameToEng = (category: string) => {
+  switch (category) {
+    case '모각코':
+      return 'mogakco';
+    case '취미':
+      return 'hobby';
+    case '스터디':
+      return 'study';
+    case '사이드 프로젝트':
+      return 'side-project';
+    default:
+      return 'mogakco';
+  }
+};
