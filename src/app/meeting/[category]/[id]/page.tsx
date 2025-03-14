1595084391045aa62de066bb2d0680282b9bd359
dbswl701 <dbswl701@ajou.ac.kr>
2025-03-14 23:25:11 +0900

    feat(DEVING-81): 모임 상세 조회에 prefetch 설정

--- src/app/meeting/[category]/[id]/page.tsx
+++ src/app/meeting/[category]/[id]/page.tsx
@@ -23,48 +23,47 @@ import ReviewInput from '../../_features/ReviewInput';
 import ReviewList from '../../_features/ReviewList';
 import UserInfo from '../../_features/UserInfo';
 
-export default function page({ params }: { params: { id: string } }) {
+export default async function page({ params }: { params: { id: string } }) {
   const meetingId = parseInt(params.id);
+  const queryClient = new QueryClient();
 
-  // const queryClient = new QueryClient();
-
-  // await Promise.all([
-  //   queryClient.prefetchQuery({
-  //     queryKey: meetingKeys.detailInfo(meetingId),
-  //     queryFn: () => getMeetingDetail(meetingId),
-  //   }),
-  //   queryClient.prefetchQuery({
-  //     queryKey: meetingKeys.detailInfoUser(meetingId),
-  //     queryFn: () => getMeetingDetailManager(meetingId),
-  //   }),
-  //   queryClient.prefetchQuery({
-  //     queryKey: commentKeys.count(meetingId),
-  //     queryFn: () => getCommentsCount(meetingId),
-  //   }),
-  //   queryClient.prefetchInfiniteQuery({
-  //     queryKey: commentKeys.comments(meetingId),
-  //     queryFn: ({ pageParam }) => getCommentsMeeting(meetingId, pageParam),
-  //     getNextPageParam: (lastPage: Paginated<Comments>) =>
-  //       lastPage.nextCursor ?? false,
-  //     initialPageParam: 0,
-  //   }),
-  // ]);
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: meetingKeys.detailInfo(meetingId),
+      queryFn: () => getMeetingDetail(meetingId),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: meetingKeys.detailInfoUser(meetingId),
+      queryFn: () => getMeetingDetailManager(meetingId),
+    }),
+    queryClient.prefetchQuery({
+      queryKey: commentKeys.count(meetingId),
+      queryFn: () => getCommentsCount(meetingId),
+    }),
+    queryClient.prefetchInfiniteQuery({
+      queryKey: commentKeys.comments(meetingId),
+      queryFn: ({ pageParam }) => getCommentsMeeting(meetingId, pageParam),
+      getNextPageParam: (lastPage: Paginated<Comments>) =>
+        lastPage.nextCursor ?? false,
+      initialPageParam: 0,
+    }),
+  ]);
 
   return (
     <div className="flex flex-col gap-[48px]">
       <FloatingButtonGroup />
       <BackButton />
-      {/* <HydrationBoundary state={dehydrate(queryClient)}> */}
-      <div className="flex flex-col gap-[48px] p-[16px] md:px-[48px]">
-        <CardWrapper meetingId={meetingId} />
-        <UserInfo meetingId={meetingId} />
-        <MeetingInfo meetingId={meetingId} />
-        <ContentLabel>리뷰</ContentLabel>
-        <ReviewAvgCard meetingId={meetingId} />
-        <ReviewInput meetingId={meetingId} />
-        <ReviewList meetingId={meetingId} />
-      </div>
-      {/* </HydrationBoundary> */}
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <div className="flex flex-col gap-[48px] p-[16px] md:px-[48px]">
+          <CardWrapper meetingId={meetingId} />
+          <UserInfo meetingId={meetingId} />
+          <MeetingInfo meetingId={meetingId} />
+          <ContentLabel>리뷰</ContentLabel>
+          <ReviewAvgCard meetingId={meetingId} />
+          <ReviewInput meetingId={meetingId} />
+          <ReviewList meetingId={meetingId} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 }
