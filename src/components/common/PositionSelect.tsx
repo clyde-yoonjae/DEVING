0b622f236500bf61a907f27a98b5cc7250f0cf80
dbswl701 <dbswl701@ajou.ac.kr>
2025-03-16 01:43:53 +0900

    fix(DEVING-86): 회원가입 시 포지션 한글로 수정

--- src/components/common/PositionSelect.tsx
+++ src/components/common/PositionSelect.tsx
@@ -12,21 +12,21 @@ export const PositionSelect = ({
       <Chip
         className={`flex-1 hover:cursor-pointer`}
         isActive={position === 'Frontend'}
-        onClick={() => setPosition('Frontend')}
+        onClick={() => setPosition('프론트엔드')}
       >
         프론트엔드
       </Chip>
       <Chip
         className={`flex-1 hover:cursor-pointer`}
         isActive={position === 'Backend'}
-        onClick={() => setPosition('Backend')}
+        onClick={() => setPosition('백엔드')}
       >
         백엔드
       </Chip>
       <Chip
         className={`flex-1 hover:cursor-pointer`}
         isActive={position === 'Designer'}
-        onClick={() => setPosition('Designer')}
+        onClick={() => setPosition('디자이너')}
       >
         디자이너
       </Chip>
