4850cb32c17a7f4d6bf9d99c0a9864af6a26bfcb
dbswl701 <dbswl701@ajou.ac.kr>
2025-03-16 17:46:36 +0900

    fix(DEVING-86): 로그인 유효성 검사 수정

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -50,16 +50,8 @@ export const positionValidation = {
 // 로그인 유효성 검사
 export const loginEmailValidation = {
   required: '이메일을 입력해주세요.',
-  pattern: {
-    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    message: '올바른 이메일 형식이 아닙니다.',
-  },
 };
 
 export const loginPasswordValidation = {
   required: '비밀번호를 입력해주세요.',
-  minLength: {
-    value: 6,
-    message: '비밀번호는 최소 6자 이상이어야 합니다.',
-  },
 };
