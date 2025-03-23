688bf21c89f54abffe44549b07ad25f86f57a857
clyde-yoonjae <leewh77777@gmail.com>
2025-03-24 02:33:23 +0900

    Comment 타입해결(내장타입 주의)

--- src/service/api/comment.ts
+++ src/service/api/comment.ts
@@ -2,6 +2,7 @@ import axiosInstance from '@/lib/axios/axiosInstance';
 import { ApiResponse } from 'type-clyde/common/api';
 import { Paginated } from 'type-clyde/common/pagination';
 import { ICommentsCount } from 'type-clyde/meeting/comment';
+import { Comment } from 'type-clyde/meeting/comment';
 
 import { commentURL } from './endpoints';
 
